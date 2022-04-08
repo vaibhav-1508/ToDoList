198e196c92136c85f33fe42e01e2e7b59f04315c
vaibhav-1508 <vaibhav.tripathi.mst21@iitbhu.ac.in>
2022-04-08 22:33:21 +0530

    first commit

---
+++ console.js
@@ -0,0 +1,8 @@
+// user click on the add button to add text in list
+document.getElementById('add').addEventListener('click', function(){
+    var value=document.getElementById('item').value;
+    if(value){
+        
+    }
+    
+});
